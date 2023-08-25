9394febc74e60eab1ccdbc2ed80631d865ba7ca9
babisque <rodrigobabisque1@gmail.com>
2023-08-25 11:27:52 -0300

    improve the operations

--- calc.c
+++ calc.c
@@ -23,14 +23,19 @@ int main()
         result = num1 - num2;
         break;
     case '/':
+        if (num2 == 0)
+        {
+            printf("Division by zero is not allowed.\n");
+            return 1;
+        }
         result = num1 / num2;
         break;
     case '*':
         result = num1 * num2;
         break;
     default:
-        printf("Operation unavaible.");
-        break;
+        printf("Invalid operation: %c\n", op);
+        return 1;
     }
 
     printf("%d %c %d = %d\n", num1, op, num2, result);
