401adab6f0ed9ee265cdb03dcd02a48dbc15a8b4
babisque <rodrigobabisque1@gmail.com>
2023-08-25 10:12:02 -0300

    simple operations is done

--- calc.c
+++ calc.c
@@ -4,5 +4,36 @@
 
 int main()
 {
-    
+    int num1, num2, result;
+    char op;
+
+    printf("Enter the first number: ");
+    scanf("%d", &num1);
+    printf("Enter the second number: ");
+    scanf("%d", &num2);
+    printf("Digite the operation: ");
+    scanf(" %c", &op);
+
+    switch (op)
+    {
+    case '+':
+        result = num1 + num2;
+        break;
+    case '-':
+        result = num1 - num2;
+        break;
+    case '/':
+        result = num1 / num2;
+        break;
+    case '*':
+        result = num1 * num2;
+        break;
+    default:
+        printf("Operation unavaible.");
+        break;
+    }
+
+    printf("%d %c %d = %d\n", num1, op, num2, result);
+
+    return 0;
 }
