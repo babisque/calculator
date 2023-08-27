8ad03f118abeb618a562841b4cf9ef86aca9c336
babisque <rodrigobabisque1@gmail.com>
2023-08-27 18:33:49 -0300

    now the calculator can do simple operations

--- calc.c
+++ calc.c
@@ -1,44 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 
 int main()
 {
     int num1, num2, result;
     char op;
 
-    printf("Enter the first number: ");
+    printf("Enter a number: ");
     scanf("%d", &num1);
-    printf("Enter the second number: ");
-    scanf("%d", &num2);
-    printf("Digite the operation: ");
-    scanf(" %c", &op);
-
-    switch (op)
+    
+    while (1)
     {
-    case '+':
-        result = num1 + num2;
-        break;
-    case '-':
-        result = num1 - num2;
-        break;
-    case '/':
-        if (num2 == 0)
+        printf("Type the operation (or '=' to finish): ");
+        scanf(" %c", &op);
+
+        if (op == '=')
+            break;
+        
+        printf("Enter another number: ");
+        scanf("%d", &num2);
+
+        switch (op)
         {
-            printf("Division by zero is not allowed.\n");
-            return 1;
+            case '+':
+                num1 = num1 + num2;
+                break;
+            case '-':
+                num1 = num1 - num2;
+                break;
+            case '/':
+                if (num2 == 0)
+                {
+                    printf("Division by zero is not allowed.\n");
+                    return 1;
+                }
+                num1 = num1 / num2;
+                break;
+            case '*':
+                num1 = num1 * num2;
+                break;
+            default:
+                printf("Invalid operation: %c\n", op);
+                return 1;
         }
-        result = num1 / num2;
-        break;
-    case '*':
-        result = num1 * num2;
-        break;
-    default:
-        printf("Invalid operation: %c\n", op);
-        return 1;
     }
 
-    printf("%d %c %d = %d\n", num1, op, num2, result);
+    printf("The result is %d\n", num1);
 
     return 0;
 }
